e78a47875e1037c2492490b7c464a1c3095a6f42
PWR <pwrpntr@gmail.com>
2024-12-19 21:43:33 +0100

    add intro text

---
+++ src/lib/components/IntroText.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { renderBlockText } from "$lib/modules/sanity.js"
+  export let pageList: any
+</script>
+
+<div class="intro-text">
+  {@html renderBlockText(pageList?.introText?.content ?? [])}
+</div>
+
+<style lang="scss">
+  .intro-text {
+    margin-top: 1.5em;
+    max-width: 50ch;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
   import Header from "$lib/components/Header.svelte"
   import List from "$lib/components/List/List.svelte"
   import Metadata from "$lib/components/Metadata.svelte"
+  import IntroText from "$lib/components/IntroText.svelte"
   export let data
   const { siteLastUpdated, pageList, itemList } = data
 </script>
 
 <Metadata globalPage={pageList} />
 <Header pageList={pageList.pages} {siteLastUpdated} />
+<IntroText {pageList} />
 <List {itemList} />
